ba27a2b38c4e19faaa85c9d1c70dbb4c341f0c5b
Denner <dennerj@gmail.com>
2016-10-24 15:34:25 -0700

    first round of cleanup
    added es-lint config, used use strict. changes function naming case

--- device-scale.js
+++ device-scale.js
@@ -1,6 +1,7 @@
+"use strict";
 
 function DevicePower(state) {
-    player = GetPlayer();
+    var player = GetPlayer();
     var devicePower = player.SetVar("devicePower", state);
     devicePower = player.GetVar("devicePower");
     switch (devicePower) {
@@ -16,15 +17,16 @@ function DevicePower(state) {
 }
 
 function ToggleDeviceUnits() {
+    var player = GetPlayer();
     var currentUnits = player.GetVar("deviceUnits");
     switch (currentUnits) {
         case "lbs":
             player.SetVar("deviceUnits", "kgs");
-            SetDisplayValue(player.GetVar("netWeightKgs"));
+            setDisplayValue(player.GetVar("netWeightKgs"));
             break;
         case "kgs":
             player.SetVar("deviceUnits", "lbs");
-            SetDisplayValue(player.GetVar("netWeightLbs"));
+            setDisplayValue(player.GetVar("netWeightLbs"));
             break;
         default:
             player.SetVar("deviceUnits", "x_x");
@@ -32,23 +34,25 @@ function ToggleDeviceUnits() {
 }
 
 function Weigh() {
+    var player = GetPlayer();
     GetWeight();
     player.SetVar("netWeightLbs", parseFloat(player.GetVar("weightLbs")) + parseFloat(player.GetVar("tareLbs")));
     player.SetVar("netWeightKgs", parseFloat(player.GetVar("weightKgs")) + parseFloat(player.GetVar("tareKgs")));
     var currentUnits = player.GetVar("deviceUnits");
     switch (currentUnits) {
         case "lbs":
-        SetDisplayValue(player.GetVar("netWeightLbs"));
-        break;
+            setDisplayValue(player.GetVar("netWeightLbs"));
+            break;
         case "kgs":
-        SetDisplayValue(player.GetVar("netWeightKgs"));
-        break;
+            setDisplayValue(player.GetVar("netWeightKgs"));
+            break;
         default:
-        player.SetVar("deviceUnits", "x_x");
+            player.SetVar("deviceUnits", "x_x");
     }
 }
 
 function GetWeight() {
+    var player = GetPlayer();
     var containerWeight = parseFloat(player.GetVar("containerState")) * parseFloat(player.GetVar("containerWeight"));
     var lrgBMC = parseFloat(player.GetVar("lrgBMCState")) * parseFloat(player.GetVar("lrgBMC"));
     var medBMC = parseFloat(player.GetVar("medBMCState")) * parseFloat(player.GetVar("medBMC"));
@@ -66,12 +70,13 @@ function tempDrop() {
 }
 
 function SetTare() {
+    var player = GetPlayer();
     player.SetVar("tareKgs", parseFloat(player.GetVar("weightKgs")) * -1);
     player.SetVar("tareLbs", parseFloat(player.GetVar("weightLbs")) * -1);
     Weigh();
 }
 
-function SetDisplayValue(value) {
+function setDisplayValue(value) {
+    var player = GetPlayer();
     player.SetVar("displayWeight", value);
 }
-
